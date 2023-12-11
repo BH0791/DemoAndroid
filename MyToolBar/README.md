3ad41fce9d419aa8ef5c82cb4875a7ff297df1f1
HamidB <ejecteur@yahoo.fr>
2023-12-11 23:08:06 +0100

    Update

--- MyToolBar/README.md
+++ MyToolBar/README.md
@@ -1,2 +1,4 @@
->># Toolbar & ViewPage2
->Une barre d'outils standard à utiliser dans le contenu de l'application et une vues à balayer. Avec les colors et styles personalisable dans les fichiers xlm.
+## Toolbar
+
+Une barre d'outils standard à utiliser dans le contenu de l'application. Avec les colors et styles personalisable dans les fichiers xlm.
+
--- MyToolBar/src/main/java/fr/hamtec/mytoolbar/MainActivity.java
+++ MyToolBar/src/main/java/fr/hamtec/mytoolbar/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -20,7 +21,16 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding( systemBars.left, systemBars.top, systemBars.right, systemBars.bottom );
             return insets;
         } );
-        //? Pour qu’elle soit considérée comme une ActionBar, vous devez le dire explicitement à votre Activity qui doit obligatoirement hériter de AppCompatActivity
+        
+        
+        //? Pour qu’elle soit considérée comme une ActionBar, vous devez le dire explicitement à votre Activity qui doit
+        //? obligatoirement hériter de AppCompatActivity
+        //! Plus long
+        // Toolbar toolbar = findViewById(R.id.toolbar); // obtenir la référence de la barre d'outils
+        // setSupportActionBar(toolbar); // Définir/remplacer la barre d'outils par la barre d'action
+        //! Forme plus courte
         setSupportActionBar(findViewById(R.id.toolbar));
+        
+        
     }
 }
